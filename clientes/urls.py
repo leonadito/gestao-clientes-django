5a1db3db4f4b3c34759f29a00738d9325ede76c4
Leonardo Duarte <leonardoduarte@MacBook-Pro-de-Leonardo.local>
2020-02-09 09:54:32 -0300

    Primeiro commit

---
+++ clientes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import persons_list, person_new, person_update, person_delete
+
+
+urlpatterns = [
+    path('list/', persons_list, name="person_list"),
+    path('new/', person_new, name="person_new"),
+    path('update/<int:id>/', person_update, name="person_update"),
+    path('delete/<int:id>/', person_delete, name="person_delete"),
+    ]
